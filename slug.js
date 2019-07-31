d27d720bd06da38856bede83a933b2f45c978cb5
Zakery Clarke <zakerytclarke@gmail.com>
2019-07-30 21:17:46 -0600

    Working Tree; needs infix

--- slug.js
+++ slug.js
@@ -6,20 +6,22 @@ stdio.println(test(5));
 test(n)={
   stdio.println(n);
   if(n>=10){
-    stdio.println("test");
-    return 10;
+    stdio.println("test"+"1");
+    return(10);
   }else{
     stdio.println("test");
-    return 10-n*5;
+    return(10-n*add(5,1));
   }
 };
 
 add(b,c)={
-  return b+c;
+  return(b+c);
 };
 
 `;
 
+
+var code="1+2*3/(5-4)"
 /*
 var code=
 `
@@ -46,7 +48,7 @@ if(n>=10){
 var separators=[" ","\n","\t","\r",";",",","else"];
 var blocks=["(",")","[","]","{","}"];
 var functions=["if"];
-var inline=["==",">=","<=",">","<","!=","++","^","*","/","+","-","="];//Lower Index = Higher Prioriy
+var inline=[":=","==",">=","<=",">","<","!=","=","++","^","*","/","+","-"];//Lower Index = Higher Prioriy
 var symbols=separators.concat(blocks.concat(inline));
 
 //Check for longest symbols first
@@ -75,6 +77,20 @@ while(i<code.length){
   i++;
 }
 
+//Fix Paren Mismatch
+var oldLexical=lexical;
+var lexical=[];
+
+for(var i=0;i<oldLexical.length;i++){
+  if(blocks.indexOf(oldLexical[i][0])!=-1){
+    lexical.push(oldLexical[i][0]);
+    lexical.push(oldLexical[i].substring(1));
+  }else{
+    lexical.push(oldLexical[i]);
+  }
+}
+
+
 lexical=lexical.filter(function(x){
   if(separators.indexOf(x)!=-1){
     return false;
@@ -91,18 +107,16 @@ lexical=lexical.map(function(x){
   }
 })
 
-lexical.filter(function(x){
-  if(x==""||x==null){
+lexical=lexical.filter(function(x){
+  if(x==''||x==null){
     return false;
   }else{
     return true;
   }
-})
-
-console.log(lexical);
+});
 
 
-
+//Create Parse Tree
 var root=new Tree("root");
 var current=root;
 
@@ -118,15 +132,17 @@ for(var i=0;i<lexical.length;i++){
   }
 }
 
-
+console.log(lexical);
 
 
 root.view();
 var oldRoot=root;
-root=pairFunctions(oldRoot);
+root=pairFunctions(oldRoot);//Pair Functions with arguments
 var oldRoot=root;
-root=removeNulls(oldRoot);
+root=removeNulls(oldRoot);//Trim uncessary grouping
+var oldRoot=root;
 //root=convertStacktoTree(oldRoot);
+assignParent(root);
 console.log("***********************************");
 root.view();
 
@@ -155,7 +171,8 @@ function Tree(value){
       out+=" ";
     }
     out+="|_";
-    console.log(out+this.value);
+      console.log(out+this.value);
+
     for(var i=0;i<this.children.length;i++){
       this.children[i].view(height+1);
     }
@@ -212,86 +229,14 @@ function removeNulls(current){
 }
 
 
-/*
-function pairFunctions(current){
-  var stack=[];
-  var i=0;
-  while(i<current.children.length){
-    if(current.children[i].value=="null"){
-      if(stack.length>0){
-        var temp=stack.pop();
-        temp.add(current.children[i]);
-        stack.push(temp);
-      }else{
-        stack.push(current.children[i]);
-      }
-    }else
-    if(inline.indexOf(current.children[i].value)!=-1){//Operator
-      var temp=stack.pop();
-      var temp1=new Tree(current.children[i].value);
-      temp1.add(temp);
-      i++;
-      temp1.add(current.children[i]);
-      stack.push(temp1);
-    }else{
-      stack.push(current.children[i]);
-    }
-
-    i++;
-  }
-
-
-  var out=new Tree(current.value);
-  for(let i=0;i<stack.length;i++){
-    out.add(stack[i]);
-  }
-
-  for(let i=0;i<out.children.length;i++){
-    out.children[i]=pairFunctions(out.children[i]);
-  }
-
-
-  return out;
-}
-*/
-
-
-/*
-function pairFunctions(current){
-  var out=new Tree(current.value);
-
-  var prev=0;
-
-  for(let i=0;i<current.children.length;i++){
-    if(current.children[i].value=="null"){
-      if(out.children.length==0){
-        out.add(current.children[i]);
-      }else{
-        out.children[prev].add(current.children[i]);
-        prev=out.children.length-1;
-      }
-    }else{
-      out.add(current.children[i]);
-      prev=out.children.length-1;
-
-    }
-  }
-
-
-
-  for(let i=0;i<out.children.length;i++){
-    out.children[i]=pairFunctions(out.children[i]);
-  }
-
-  return out;
-}
-*/
 
 function convertStacktoTree(current){
   var stack=[];
   var out=[];
 
   for(var i=0;i<current.children.length;i++){
+    console.log(out);
+    console.log("*******************");
     if(inline.indexOf(current.children[i].value)==-1){
       out.push(current.children[i]);
     }else
@@ -310,19 +255,36 @@ function convertStacktoTree(current){
     }
   }
 
+
     while(stack.length>0){
       out.push(stack.pop());
     }
 
   var outTree=new Tree(current.value);
 
+  for(var i=0;i<out.length;i++){
+    outTree.add(out[i]);
+  }
 
+
+  for(var i=0;i<outTree.children.length;i++){
+    outTree.children[i]==convertStacktoTree(outTree.children[i]);
+  }
+  outTree.view();
+  return outTree;
 }
 
 
 
 
-
+function assignParent(current){
+  for(var i=0;i<current.children.length;i++){
+    current.children[i].parent=current;
+  }
+  for(var i=0;i<current.children.length;i++){
+    assignParent(current.children[i]);
+  }
+}
 
 
 
